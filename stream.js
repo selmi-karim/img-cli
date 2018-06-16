0a2d55622d8da32587953d279db8fb7642114d08
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-16 07:34:22 +0100

    imgbuffer function

---
+++ stream.js
@@ -0,0 +1,18 @@
+
+const got = require('got')
+const imageType = require('image-type')
+const isUrl = require('is-url-superb')
+
+
+ const imageBuffer = async(url) => {
+    if (!(url && isUrl(url))) throw new TypeError('A valid url is required')
+
+    const response = await got(url, { encoding: null })
+    const buffer = Buffer.from(response.body, 'binary')
+    const type = imageType(buffer)
+
+    if (!type) return null
+    return buffer
+}
+
+module.exports = {  imageBuffer };
