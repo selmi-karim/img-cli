14e941bd2b3c351090b4fe1ed807e2c2c0f3d2cf
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-18 11:25:57 +0100

    refactor code :hammer:

--- src/download.js
+++ src/download.js
@@ -1,6 +1,7 @@
 const fs = require('fs') 
 const imageType = require('image-type') 
 const stream = require('./stream.js') 
+
 stream.imageBuffer('https://www.fillmurray.com/g/200/300').then(buffer => {
     const type = imageType(buffer) 
     fs.writeFile('bill-murray.' + type.ext, buffer, (err) => console.log(err ? err : 'done!')) 
