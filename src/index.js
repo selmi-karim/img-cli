df48570157d52fb334cc55f6f9ef58dc4d2a88a0
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-18 14:41:38 +0100

    solo img :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict' 
 
-const program = require('commander');
-const crawler = require('./crawler.js');
+const program = require('commander') 
+const crawler = require('./crawler.js') 
+const download = require('./download.js')
+
 const isURL = (url) => {
     const pattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
     return pattern.test(url)
@@ -11,23 +13,42 @@ const isURL = (url) => {
 
 program
     .version('1.0.0', '-v, --version')
-    .description('Images Downloader');
+    .description('Images Downloader') 
 
 program
-    .command('img <url>')
+    .command('all <url>')
     .description('download all images from url with optional directory name and specific image type')
     .option('-d, --directory [directory]', 'choose a specific directory name')
-    .option("-t, --type [type]", "Which image type")
+    .option("-t, --type [type]", "Choose a image type (png, jpg, gif..)")
     .action((url, options) => {
         const type = options.type || "all"
-        const directory = options.directory || "images/"
+        const directory = options.directory || "images"
         if (!isURL(url)) {
             console.log('error')
         }
         else {
             crawler.getImage(url,type,directory)
         }
-    });
+}) 
 
 
-program.parse(process.argv);
+program
+    .command('solo <url>')
+    .description('download spefic image')
+    .option('-d, --directory [directory]', 'choose a specific directory name')
+    .action((url, options) => {
+        const type = options.type || "all"
+        const directory = options.directory || "images"
+        if (!isURL(url)) {
+            console.log('error')
+        }
+        else {
+            //crawler.getImage(url,type,directory)
+            download.saveImg(directory, 'solo', url
+                , (response) => {
+                  //console.log('response: ' + response)
+            })
+        }
+}) 
+
+program.parse(process.argv) 
