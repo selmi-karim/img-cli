3d54fb1f7abf30655f2ead19f5c1e04820f3aa52
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-18 17:02:32 +0100

    test buffer return :boom:

--- src/download.js
+++ src/download.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: kerim selmi 
+ * @Date: 2018-06-18 16:54:23 
+ * @Last Modified by:   kerim selmi 
+ * @Last Modified time: 2018-06-18 16:54:23 
+ */
 const fs = require('fs')
 const mkdirp = require('mkdirp')
 var getDirName = require('path').dirname
 const imageType = require('image-type')
 const stream = require('./stream.js')
+const styles = require('./styles')
+
 
 const saveImg = (directory, name, imgURL,callback) => {
     stream.imageBuffer(imgURL).then(buffer => {
