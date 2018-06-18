e96f445225eb640645181fdf6fe51604131a384a
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-18 17:02:04 +0100

    fetch all elements

--- src/crawler.js
+++ src/crawler.js
@@ -1,26 +1,47 @@
-const getImageUrls = require('get-image-urls')
+/*
+ * @Author: kerim selmi 
+ * @Date: 2018-06-18 16:54:30 
+ * @Last Modified by: kerim selmi
+ * @Last Modified time: 2018-06-18 16:55:32
+ */
+const getImages = require('./img_urls')
 const download = require('./download.js')
+const styles = require('./styles')
+const ora = require('ora');
+
 
 const uid = () => {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16) + '_' + Math.floor((1 + Math.random()) * 0x10000).toString(16)
 }
 const getImage = (url, type, directory) => {
-  getImageUrls(url, function (err, images) {
+  const spinner = ora('Fetching').start()
+
+  getImages.getImageUrls(url, function (err, images) {
     if (!err) {
-      console.log('Images found', images.length);
-      images.forEach(function (element) {
-        //console.log('url: '+element)
+      spinner.succeed()
+      styles.green(images.length + ' images found');
+      styles.bar.start(images.length, 0);
+      images.forEach((element, index) => {
+        // start the progress bar with a total value of 200 and start value of 0
+
+
         const name = uid()
         download.saveImg(directory, name, element.url
           , (response) => {
+            styles.bar.increment(1)
+
+            // update the current value in your application..
             //console.log('response: ' + response)
           })
+        //if(index==images.length-1) bar.stop()
       })
     }
     else {
       console.log('ERROR', err);
     }
   })
+  //console.log('done')
+
 }
 
 module.exports = { getImage }
