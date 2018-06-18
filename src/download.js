297168c51db039d612c41263cfc7df298181bc9f
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-18 11:52:13 +0100

    img name & directory :boom::

--- src/download.js
+++ src/download.js
@@ -1,8 +1,16 @@
-const fs = require('fs') 
-const imageType = require('image-type') 
-const stream = require('./stream.js') 
+const fs = require('fs')
+const imageType = require('image-type')
+const stream = require('./stream.js')
 
-stream.imageBuffer('https://www.fillmurray.com/g/200/300').then(buffer => {
-    const type = imageType(buffer) 
-    fs.writeFile('bill-murray.' + type.ext, buffer, (err) => console.log(err ? err : 'done!')) 
-}) 
+const url = (directory,name,url) => {
+    stream.imageBuffer(url).then(buffer => {
+        const type = imageType(buffer)
+        fs.writeFile(directory+name+ type.ext, buffer, (err) => { 
+            if (err) console.log('error: '+err)
+            else {
+                console.log('done')
+            }
+        })
+    })
+}
+module.exports = { url }
