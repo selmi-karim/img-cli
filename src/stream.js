e5fa6a30329143238d7687583c2079810746c605
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-18 14:45:35 +0100

    + isurl function :bug:

--- src/stream.js
+++ src/stream.js
@@ -1,17 +1,19 @@
 
 const got = require('got')
 const imageType = require('image-type')
-const isUrl = require('is-url-superb')
 
- const imageBuffer = async(url) => {
-    if (!(url && isUrl(url))) throw new TypeError('A valid url is required')
+const isURL = (url) => {
+    const pattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+    return pattern.test(url)
+}
+const imageBuffer = async (url) => {
+    if (!(url && isURL(url))) throw new TypeError('A valid url is required')
 
     const response = await got(url, { encoding: null })
     const buffer = Buffer.from(response.body, 'binary')
     const type = imageType(buffer)
-
     if (!type) return null
     return buffer
 }
 
-module.exports = {  imageBuffer };
+module.exports = { imageBuffer };
