d12faa4e41217663a8b81dce696b0572edd52ba4
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-19 10:00:48 +0100

    buffer test

--- src/download.js
+++ src/download.js
@@ -1,8 +1,8 @@
 /*
  * @Author: kerim selmi 
  * @Date: 2018-06-18 16:54:23 
- * @Last Modified by:   kerim selmi 
- * @Last Modified time: 2018-06-18 16:54:23 
+ * @Last Modified by: kerim selmi
+ * @Last Modified time: 2018-06-18 23:03:19
  */
 const fs = require('fs')
 const mkdirp = require('mkdirp')
@@ -12,17 +12,50 @@ const stream = require('./stream.js')
 const styles = require('./styles')
 
 
-const saveImg = (directory, name, imgURL,callback) => {
+const saveImg = (directory, name, imgURL) => {
     stream.imageBuffer(imgURL).then(buffer => {
         const type = imageType(buffer)
         const path = directory + '/' + name + '.' + type.ext
         mkdirp(getDirName(path), function (err) {
-            if (err) callback(err)
+            if (err) {
+                styles.red(err)
+                return err
+            }
             fs.writeFile(path, buffer, (err) => {
-                if (err) callback(err)
-                return callback('done')
+                if (err) {
+                    styles.red(err)
+                    return err
+                }
+                styles.bar.increment(1)
             })
         })
     })
+    return true
 }
-module.exports = { saveImg }
+
+
+const solo = (directory, name, imgURL) => {
+    stream.imageBuffer(imgURL).then(buffer => {
+        const type = imageType(buffer)
+        if(buffer===null) {
+            styles.red('try to use valid url')
+            return null
+        }
+        const path = directory + '/' + name + '.' + type.ext
+        mkdirp(getDirName(path), function (err) {
+            if (err) {
+                styles.red(err)
+                return err
+            }
+            fs.writeFile(path, buffer, (err) => {
+                if (err) {
+                    styles.red(err)
+                    return err
+                }
+                styles.bar.increment(1)
+            })
+        })
+    })
+    return true
+}
+module.exports = { saveImg,solo }
