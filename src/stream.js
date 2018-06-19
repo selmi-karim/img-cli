fd98dad755d82149c27a605144b38ddf7fbe6809
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-19 12:08:48 +0100

    + isurl function :bug:

--- src/stream.js
+++ src/stream.js
@@ -2,7 +2,7 @@
  * @Author: kerim selmi 
  * @Date: 2018-06-18 16:54:32 
  * @Last Modified by: kerim selmi
- * @Last Modified time: 2018-06-18 22:57:45
+ * @Last Modified time: 2018-06-19 12:08:19
  */
 
 const got = require('got')
@@ -15,7 +15,6 @@ const isURL = (url) => {
 }
 const imageBuffer = async (url) => {
     if (!(url && isURL(url))) throw new TypeError('A valid url is required')
-
     const response = await got(url, { encoding: null })
     const buffer = Buffer.from(response.body, 'binary')
     const type = imageType(buffer)
